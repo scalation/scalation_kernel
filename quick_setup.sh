8bd6cc328b8c629c800a53cdc82c2512104761da
Michael Cotterell <mepcotterell@gmail.com>
2017-11-06 17:46:31 -0500

    fixed typo in quick setup

--- quick_setup.sh
+++ quick_setup.sh
@@ -8,7 +8,7 @@ if [ ! -d "$VIRTUALENV" ]; then
 fi
 
 # activate virtualenv
-source bin/activate
+source "$VIRTUALENV/bin/activate"
 
 # download ScalaTion if needed
 if [ ! -d "scalation_1.3" ]; then
@@ -20,13 +20,14 @@ fi
 export SCALATION_MATHSTAT_JAR="$(pwd)/scalation_1.3/scalation_models/lib/scalation_mathstat_2.12-1.3.jar"
 export SCALATION_MODELING_JAR="$(pwd)/scalation_1.3/scalation_models/lib/scalation_modeling_2.12-1.3.jar"
 
-# install jupyter and notebook if needed
+# install python packages if needed
 python3 -m pip install --upgrade pip
 python3 -m pip install jupyter
+
+# install kernel
 python3 -m pip install -e .
 python3 -m scalation_kernel.install
 
 # run jupyter
 python3 -m jupyter notebook
-
 
